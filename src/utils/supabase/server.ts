ff5673766975abaf7b90f389b365731606d7e77e
aryeh_samoka <aryeh.samoka@feedvisor.com>
2026-04-10 12:47:37 +0300

    hard code 3

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -4,12 +4,12 @@ import { cookies } from 'next/headers'
 export async function createSupabaseServerClient() {
   const cookieStore = await cookies()
   
-  const FALLBACK_SUPABASE_URL = 'https://aklaifdmrdloycebatrd.supabase.co'
-  const FALLBACK_SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFrbGFpZmRtcmRsb3ljZWJhdHJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU2NzgxOTMsImV4cCI6MjA5MTI1NDE5M30.2kAu8T0LujyOgeC0bfanizS4Mj2-tuQ_M24tGOpqZu4'
+  // const FALLBACK_SUPABASE_URL = 'https://aklaifdmrdloycebatrd.supabase.co'
+  // const FALLBACK_SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFrbGFpZmRtcmRsb3ljZWJhdHJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU2NzgxOTMsImV4cCI6MjA5MTI1NDE5M30.2kAu8T0LujyOgeC0bfanizS4Mj2-tuQ_M24tGOpqZu4'
 
 
-  const supabaseUrl = FALLBACK_SUPABASE_URL
-  const supabaseAnonKey = FALLBACK_SUPABASE_ANON_KEY
+  const supabaseUrl = 'https://aklaifdmrdloycebatrd.supabase.co'
+  const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFrbGFpZmRtcmRsb3ljZWJhdHJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU2NzgxOTMsImV4cCI6MjA5MTI1NDE5M30.2kAu8T0LujyOgeC0bfanizS4Mj2-tuQ_M24tGOpqZu4'
 
   if (!supabaseUrl || !supabaseAnonKey) {
     throw new Error('Supabase environment variables are missing. Please check your production dashboard.')
