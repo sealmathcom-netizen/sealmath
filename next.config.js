873f2581acb6f4c72e90c8168960a5cbb3000670
aryeh_samoka <aryeh.samoka@feedvisor.com>
2026-04-15 12:18:01 +0300

    fix attachment to 5 second try

--- next.config.js
+++ next.config.js
@@ -3,8 +3,13 @@ import { MAX_ATTACHMENT_SIZE_MB } from './src/utils/shared-config.js'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  serverActions: {
-    bodySizeLimit: `${MAX_ATTACHMENT_SIZE_MB * 1.25}mb`,
+  experimental: {
+    serverActions: {
+      bodySizeLimit: `${MAX_ATTACHMENT_SIZE_MB * 1.25}mb`,
+    },
+  },
+  env: {
+    MAX_ATTACHMENT_SIZE_MB: String(MAX_ATTACHMENT_SIZE_MB),
   },
 }
 
